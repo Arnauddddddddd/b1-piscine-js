61cee5b3f3fc73923b541d766791e9f282cb4b1f
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 18:17:25 +0100

    neuvieme commit

--- is.js
+++ is.js
@@ -20,7 +20,7 @@ is.arr = function(v) {
     return Array.isArray(v);
 }
 is.obj = function(v) {
-    return typeof{}(v);
+    return typeof(v) === typeof new Object;
 }
 is.fun = function(v) {
     return typeof{}(v);
