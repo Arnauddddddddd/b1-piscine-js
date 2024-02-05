529f121071f9b2be3bc6e29fccc4455c960724f4
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 12:23:38 +0100

    septieme commit

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words(word) {
+    return word.split(' ');
+}
+
+function sentence(arr) {
+    return arr.join('')
+}
+
+function yell(word) {
+    return word.toUpperCase()
+}
+
+function whisper(word) {
+    return word.toLowerCase()
+}
+
+function capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1);
+}
