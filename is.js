88c3c9ba42b1cad2f20baf91f49ebfa8e0eca680
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 17:37:07 +0100

    neuvieme commit

--- is.js
+++ is.js
@@ -5,7 +5,7 @@ is.str = function(v) {
     return typeof(v) == String;
 }
 is.nan = function(v) {
-    return isNaN(v);
+    return !isNumber(v);
 }
 is.bool = function(v) {
     return typeof(v) == Boolean;
