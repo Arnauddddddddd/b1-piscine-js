ec8c245a8398adffb0f2beb4a0bfb89a6d56a065
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 13:02:30 +0100

    huitieme commit

---
+++ is.js
@@ -0,0 +1,34 @@
+is.num = function(v) {
+    return typeof(v) == Number;
+}
+is.str = function(v) {
+    return typeof(v) == String;
+}
+is.nan = function(v) {
+    return v == NaN;
+}
+is.bool = function(v) {
+    return typeof(v) == Boolean;
+}
+is.undef = function(v) {
+    return typeof(v) == undefined;
+}
+is.def = function(v) {
+    return typeof(v) != undefined;
+}
+is.arr = function(v) {
+    return typeof(v) == Array;
+}
+is.obj = function(v) {
+    return typeof(v) == Object;
+}
+is.fun = function(v) {
+    return typeof(v) == Function;
+}
+is.truthy = function(v) {
+    return v == true;
+}
+is.falsy = function(v) {
+    return v == false;
+}
+
