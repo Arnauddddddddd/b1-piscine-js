5d1e69af34238b84822587ea7c6a92bdb4c6399b
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 00:54:49 +0100

    2ème module

--- elementary.js
+++ elementary.js
@@ -1,7 +1,12 @@
 function multiply(a, b) {
     var result = 0;
+    var sign = 1;
+    if (b < 0) {
+        abs(b);
+        sign = -1;
+    }
     for (let i = 0; i < b; i++) {
         result += a;
     } 
-    return result;
+    return result * sign;
 }
