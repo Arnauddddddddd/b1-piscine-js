97605037c712cf56c09d3bd1a3ab118c7903fd94
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 10:47:11 +0100

    quatrieme commit

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+function more(value) {
+    return value + 1;
+}
+function less(value) {
+    return value - 1;
+}
+function add(value, value2) {
+    return value + value2;
+}
+function sub(value, value2) {
+    return value - value2;
+}
