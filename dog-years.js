435ca42d8087ac92e26cc0852d580a9062b8d0c6
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 11:46:32 +0100

    sixième commit

--- dog-years.js
+++ dog-years.js
@@ -12,15 +12,15 @@ function dogYears(planet, age) {
         return parseFloat((7 * (age / (1.8808158 * 31557600))).toFixed(2));
     }
     if (planet == "jupiter") {
-        return parseFloat((7 * (age / 11.862615 * 31557600)).toFixed(2));
+        return parseFloat((7 * (age / (11.862615 * 31557600))).toFixed(2));
     }
     if (planet == "saturn") {
-        return parseFloat((7 * (age / 29.447498 * 31557600)).toFixed(2));
+        return parseFloat((7 * (age / (29.447498 * 31557600))).toFixed(2));
     }
     if (planet == "uranus") {
-        return parseFloat((7 * (age / 84.016846 * 31557600)).toFixed(2));
+        return parseFloat((7 * (age / (84.016846 * 31557600))).toFixed(2));
     }
     if (planet == "neptune") {
-        return parseFloat((7 * (age / 164.79132 * 31557600)).toFixed(2));
+        return parseFloat((7 * (age / (164.79132 * 31557600))).toFixed(2));
     }
 }
