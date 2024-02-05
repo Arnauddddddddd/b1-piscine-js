9a0c70bdd0e3c7194ef4123543b43b358d312cd3
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-05 10:40:23 +0100

    quatrieme commit

--- mutability.js
+++ mutability.js
@@ -1,5 +1,5 @@
-const clone1 = person;
-const clone2 = person;
+const clone1 = new Object(person);
+const clone2 = new Object(person);
 samePerson = Object.assign({}, person);
 person.age += 1;
 person.country = 'FR';
