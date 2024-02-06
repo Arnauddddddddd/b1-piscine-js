979c4f8c476914e8ab814d12ab53abfd433f1f18
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 19:55:10 +0100

    mon prime

--- index-of.js
+++ index-of.js
@@ -1,5 +1,5 @@
 function indexOf(arr, value, index) {
-    for (let i = index; i < arr.length; i++) {
+    for (let i = index-1; i < arr.length; i++) {
         if (arr[i] == value) {
             return i;
         }
