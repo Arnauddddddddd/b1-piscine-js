ef3ca8944e845df483a0197d071605ceaab3384c
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 21:18:45 +0100

    finito

--- slicer.js
+++ slicer.js
@@ -1,8 +1,11 @@
-function slice(arr, index, end = null) {
-    if (end == null) {
-        end = arr.length;
+function slice(arr, index, end = -1) {
+    if (end == -1) {
+        end = arr.length-1;
     }
     if (index <= end) {
         return arr[index] + slice(arr, index+1, end);
     }
+    return "";
 }
+
+console.log(slice('abcdef', 2))
