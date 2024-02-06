90f1000faeda12e667a6014e9d0b8edf3ab913d0
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 21:24:09 +0100

    finito

--- slicer.js
+++ slicer.js
@@ -2,10 +2,13 @@ function slice(arr, index, end = -1) {
     if (end == -1) {
         end = arr.length-1;
     }
+    if (index < 0) {
+        index = arr.length + index;
+    }
     if (index <= end) {
         return arr[index] + slice(arr, index+1, end);
     }
     return "";
 }
 
-console.log(slice('abcdef', 2))
+console.log(slice('abcdef', -2))
