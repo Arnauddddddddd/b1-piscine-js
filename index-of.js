a2d7589d2b5ba706fa6fd50c6c16218dadcdeb8d
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 20:43:38 +0100

    mon prime ou pas

--- index-of.js
+++ index-of.js
@@ -8,11 +8,19 @@ function indexOf(arr, value, index = 0) {
 }
 
 function lastIndexOf(arr, value, index = 0) {
-    return indexOf(arr.reverse(), value, arr.length-1 - index);
+    if (indexOf(arr.reverse().slice(0, arr.length-1 -index), value) == -1) {
+        return -1;
+    }
+    return arr.length-1 - indexOf(arr.reverse().slice(0, arr.length-1 -index), value);
 }
 
 
 function includes(arr, value, index) {
     return indexOf(arr, value, index) != -1;
 }
+
+
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
+console.log(lastIndexOf([0, 0, 2, 2], 2, 3))
+
 
