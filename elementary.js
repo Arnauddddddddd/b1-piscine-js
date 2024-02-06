4165f74cdb502390bb4203f668ffb78a0f5e83c8
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 13:36:38 +0100

    le retour

--- elementary.js
+++ elementary.js
@@ -2,14 +2,15 @@ function multiply(a, b) {
     var result = 0;
     var neg = false;
     if (a < 0) {
-        abs(a);
+        a = Math.abs(a);
         neg = true;
     }
     if (b < 0 && neg) {
-        abs(b);
+        b = Math.abs(b);
         neg = false;
     } else if (b < 0) {
         neg = true;
+        b = Math.abs(b);
     }
     for (let i = 0; i < b; i++) {
         result += a;
@@ -19,3 +20,18 @@ function multiply(a, b) {
     }
     return result;
 }
+
+function divide(a, b) {
+    var result = 0; 
+    if ((a == 0) || (b == 0)) {
+        return null;
+    }
+    for (let i = a; i < b; i++) {
+        if (a * i == b) {
+            return i;
+        }
+    }
+    return result;
+}
+
+console.log(divide(4, 20));
