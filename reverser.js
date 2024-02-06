8fd383e0235b0f0c4d5279de4a130ec3566c0ef5
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 23:36:15 +0100

    finito 2 ou pas ??!

--- reverser.js
+++ reverser.js
@@ -1,10 +1,14 @@
 function reverse(arr) {
     var newArr = [];
+    var str = "";
     for (var i = arr.length-1; i >= 0; i--) {
         newArr.push(arr[i])
+        str += arr[i];
     }
     if (Array.isArray(arr)) {
         return newArr;
     }
-    return newArr.split("");
+    return str;
 }
+
+console.log(reverse('pouet'))
