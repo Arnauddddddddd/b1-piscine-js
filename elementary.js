906c5d32c18ee48ed5f0acd4e18fc67c36d25707
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-06 18:19:05 +0100

    le retour

--- elementary.js
+++ elementary.js
@@ -1,17 +1,7 @@
 function multiply(a, b) {
     var result = 0;
     var neg = false;
-    if (a < 0) {
-        a = -a;
-        neg = true;
-    }
-    if (b < 0 && neg) {
-        b = -b;
-        neg = false;
-    } else if (b < 0) {
-        neg = true;
-        b = -b;
-    }
+    a, b, neg = negative(a, b, neg);
     for (let i = 0; i < b; i++) {
         result += a;
     } 
@@ -21,12 +11,7 @@ function multiply(a, b) {
     return result;
 }
 
-function divide(a, b) {
-    var result = 0;
-    var neg = false;
-    if ((a == 0) || (b == 0)) {
-        return null;
-    }
+function negative(a, b, neg) {
     if (a < 0) {
         a = -a;
         neg = true;
@@ -38,15 +23,30 @@ function divide(a, b) {
         neg = true;
         b = -b;
     }
-    while (a >= b) 
-    { 
-       a = a - b; 
-       result++ ; 
-    }
-    if (neg) {
-        result = -result;
-    }
-    return result;
+    return a, b, neg
 }
 
-console.log(divide(78, 32));
+function divide(a, b) {
+    var neg = false;
+    if ((a == 0) || (b == 0)) {
+        return null;
+    }
+    a, b, neg = negative(a, b, neg);
+    for (let i = 0; i < b; i++) {
+        if (multiply(i, b) == a) {
+            if (neg) {
+                i = -i;
+            }
+            return i;
+        }
+        if (multiply(i, b) > a) {
+            if (neg) {
+                i = -i+1;
+            }
+            return i-1;
+        }
+    }
+   
+}
+
+console.log(divide(78, 34));
