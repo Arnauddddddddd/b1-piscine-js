111e9a1c6b4914f91b706282031bc08793bcd63c
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-07 20:21:20 +0100

    finitoooooooooooofffffo

--- chunky.js
+++ chunky.js
@@ -1,3 +1,16 @@
 function chunk(arr, size) {
-    return arr;
+    new newArr = [];
+    new newArr2 = [];
+    numberI = arr.length / size;
+    for (var i = 0; i < number; i++) {
+        newArr2 = []
+        for (var j = 0; j < size; j++) {
+            newArr2.push(arr[i+j])
+        }
+        newArr.push(newArr2)
+    }
+    if (arr % size != 0) {
+        newArr.push([arr.slide(arr.length - (arr % size))])
+    }
+    return newArr;
 }
