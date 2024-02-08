8fc0c4f93407a188a7a7d718e175d9f6c4920b37
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-08 10:10:54 +0100

    plus finito

--- letter-space-number.js
+++ letter-space-number.js
@@ -4,12 +4,17 @@ function letterSpaceNumber(str) {
     var letter = "";
     var car = "";
     for (var i = 0; i < arrStr.length; i++) {
-        if ((arrStr[i] <= '9' && arrStr[i] >= '0') && arrStr[i].length == 1) {
+        if ((arrStr[i] <= '9' && arrStr[i] >= '0')) {
+            console.log(arrStr[i])
             letter = arrStr[i-1];
             if ((letter > "a" && letter < "z") || (letter > "A" && letter < "Z")) {
-                car = letter[letter.length-1] + " " + arrStr[i][arrStr[i].length-1]
-                arr.push(car);
-                car = "";
+                if (arrStr[i].length != 1) {
+                    if (!(arrStr[i][1] <= '9' && arrStr[i][1] >= '0')) {
+                        car = letter[letter.length-1] + " " + arrStr[i][arrStr[i].length-1]
+                        arr.push(car);
+                        car = "";
+                    }
+                }  
             }
         } 
     }
