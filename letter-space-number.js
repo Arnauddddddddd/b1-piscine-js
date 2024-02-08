51ddc8b948958ea8c0c5426e058708e60fc17e42
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-08 10:43:27 +0100

    plus finito

--- letter-space-number.js
+++ letter-space-number.js
@@ -4,11 +4,13 @@ function letterSpaceNumber(str) {
     var letter = "";
     var car = "";
     for (var i = 0; i < arrStr.length; i++) {
-        if ((arrStr[i] <= '9' && arrStr[i] >= '0')) {
+        if ((arrStr[i][0] <= '9' && arrStr[i][0] >= '0')) {
             console.log(arrStr[i])
             letter = arrStr[i-1];
-            if ((letter > "a" && letter < "z") || (letter > "A" && letter < "Z")) {
+            console.log(letter)
+            if ((letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z")) {
                 if (arrStr[i].length != 1) {
+                    console.log(arrStr[i].charCodeAt(1))
                     if ((arrStr[i].charCodeAt(1) > 57 || arrStr[i].charCodeAt(1) < 48) && (arrStr[i].charCodeAt(1) > 122 || arrStr[i].charCodeAt(1) < 97) && (arrStr[i].charCodeAt(1) > 90 || arrStr[i].charCodeAt(1) < 65)) {
                         car = letter[letter.length-1] + " " + arrStr[i][0]
                         arr.push(car);
@@ -24,9 +26,3 @@ function letterSpaceNumber(str) {
     }
     return arr;
 }
-
-console.log(letterSpaceNumber('example 1, example 2up'))
-console.log(letterSpaceNumber('Definitely 9.'))
-
-
-'Definitely 9.'
