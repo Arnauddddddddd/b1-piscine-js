891bd7a5753f27311077a11b56a7db0819bb9738
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-08 11:13:47 +0100

    plus finito qoudqfhdlsweu

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,12 +1,15 @@
 function vowelDots(str) {
+    var voyelles = new RegExp("aeiouAEIOU");
     var txt = "";
+    vowels = (str == vowelDots(str));
     for (var i = 0; i < str.length; i++) {
         txt += str[i];
-        if (str[i] in vowels) {
+        if (str[i] in voyelles) {
             txt += "."
         }
     }
+
     return txt;
 }
 
-var vowels = new RegExp("aeiouAEIOU");
+var vowels = txt;
