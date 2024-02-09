6a3f11ae712427150c2c28c2e51ada683f707937
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-09 11:34:38 +0100

    beschrelle

--- pyramid.js
+++ pyramid.js
@@ -1,11 +1,11 @@
 function pyramid(symbol, number, i = 0) {
     if (i < number) {
         if (i+1 == number) {
-            return " ".repeat(number - i-1) + symbol.repeat(i*2+1) + pyramid(symbol, number, i+1)
+            return " ".repeat((number - i-1) * symbol.length) + symbol.repeat(i*2+1) + pyramid(symbol, number, i+1)
         }
-        return " ".repeat(number - i-1) + symbol.repeat(i*2+1)  + "\n" + pyramid(symbol, number, i+1)
+        return " ".repeat((number - i-1) * symbol.length) + symbol.repeat(i*2+1)  + "\n" + pyramid(symbol, number, i+1)
     }
     return "";
 }
 
-console.log(pyramid("*", 3))
+console.log(pyramid("{}", 10))
