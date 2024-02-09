29b05e21916d6a6be592dab2c1fdaf28d37c43f8
parnaud <arnaud.pfeillefort@ynov.com>
2024-02-09 10:40:57 +0100

    aaa

--- get.js
+++ get.js
@@ -1,3 +1,14 @@
 function get(src, path) {
-    return src[path];
+    var arr = path.split(".")
+    var result;
+    var object = src;
+    for (let i = 0; i < arr.length; i++) {
+        if (i == arr.length-1) {
+            return object[arr[i]];
+        }
+        object = src[arr[i]];     
+    }
 }
+
+
+console.log(get({ nested: { key: 'value' } }, 'nested.key'))
