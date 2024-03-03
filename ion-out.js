08612272b01bb8234bd1a3e11c37a1c5a1e2f8ab
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-03 15:44:21 +0100

    salut

--- ion-out.js
+++ ion-out.js
@@ -1,11 +1,10 @@
 function ionOut(str) {
     list = str.split(" ")
     list2 = [];
-    console.log(list)
     const reg = /tion/g;
     for (var i = 0; i < list.length; i++) {
         if (list[i].match(reg) != null) {
-            list2.push(list[i])
+            list2.push(list[i].replace("ion", ""))
         }
     }
     return list2;
