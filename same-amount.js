402688568a803fb1736c81db41f135b6c88f8144
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-03 15:18:10 +0100

    salut

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,5 @@
 function sameAmount(str, reg1, reg2) {
     var regex1 = str.match(reg1);
     var regex2 = str.match(reg2);
-    return regex1 !== null && regex2 !== null && regex1.length === regex2.length;
+    return regex1 !== null && regex2 !== null && regex1.length == regex2.length;
 }
