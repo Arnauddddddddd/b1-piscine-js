6039f120d5d67ccad5eb5d8f77e522b957ecc5b3
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-03 15:19:35 +0100

    salut

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,7 @@
-function sameAmount(str, reg1, reg2) {
-    var regex1 = str.match(reg1);
-    var regex2 = str.match(reg2);
-    return regex1 !== null && regex2 !== null && regex1.length == regex2.length;
+function sameAmount(str, rexp1, rexp2) {
+    rexp1 = new RegExp(rexp1, "g");
+    rexp2 = new RegExp(rexp2, "g");
+    let test1 = str.match(rexp1);
+    let test2 = str.match(rexp2);
+    return test1 !== null && test2 !== null && test1.length === test2.length;
 }
