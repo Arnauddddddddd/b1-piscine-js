e47715b965ccb41e2e3bb316b0d5026c058f3737
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-04 10:09:57 +0100

    salutttt

--- dr-strange.js
+++ dr-strange.js
@@ -2,15 +2,10 @@ function addWeek(date) {
     var jour2 = date.getDay()
     date = date.toString()
     var jour = parseInt(date[8] + date [9])
-    console.log(jour)
     var txt = ""
     if (jour > 7 && jour <= 14) {
-        jour -= 7
         txt+= "second"
-    } else if (jour > 14 && jour <= 21) {
-        jour -= 14
     } else if (jour > 21 && jour <= 28) {
-        jour -= 21
         txt+= "second"
     }
     switch(jour2) {
@@ -40,6 +35,12 @@ function addWeek(date) {
 }
 
 console.log(addWeek(new Date('2025-08-11')))
-console.log(addWeek(new Date('0001-01-01')))
+console.log(addWeek(new Date('2001-11-07')))
+console.log(addWeek(new Date('0001-01-01'))) // Output: Monday
+console.log(addWeek(new Date('0001-01-02'))) // Output: Tuesday
+console.log(addWeek(new Date('0001-01-07'))) // Output: Sunday
+console.log(addWeek(new Date('0001-01-08'))) // Output: secondMonday
+console.log(addWeek(new Date('0001-01-09'))) // Output: secondTuesday
+
 
 
