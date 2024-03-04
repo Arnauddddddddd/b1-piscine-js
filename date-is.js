34e5a8a662faa73e9d1f2d5ca607fd00604209a7
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-04 12:29:23 +0100

    salutttt

--- date-is.js
+++ date-is.js
@@ -1,4 +1,3 @@
 function isValid(date) {
-    var pattern = new RegExp(/^\d{2}-\d{2}-\d{4}$/);
-    return (date instanceof Date && !isNaN(date)) || pattern.test(date);
+    return isValid(date);
 }
