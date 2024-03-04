055b2704951762d469c675e38b79df02574a38da
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-04 12:43:32 +0100

    salutttt

--- date-is.js
+++ date-is.js
@@ -7,3 +7,11 @@ function isValid(date) {
     }
     return date instanceof Date && !isNaN(date);
 }
+
+function isAfter(date1, date2) {
+    return date1 > date2;
+}
+
+function isBefore(date1, date2) {
+    return date1 < date2;
+}
