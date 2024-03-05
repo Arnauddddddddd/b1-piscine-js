47a417831ed71748694f0ae766bdb9056352c293
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-05 10:00:40 +0100

    salutttt

--- get-some-time.js
+++ get-some-time.js
@@ -1,9 +1,22 @@
 function firstDayWeek(number, year) {
     var date = new Date(parseInt(year), 0)
+    // for (var i = 0; i < number-1; i++) {
+    //     date.setTime(date.getTime() + (60*60*1000) * 24 * 7);
+    // }
+
     for (var i = 0; i < number-1; i++) {
-        date.setTime(date.getTime() + (60*60*1000) * 24 * 7);
+        for (var j = 0; j < 24 * 7; j++) {
+            date.setHours(date.getHours() + 1);
+        }
     }
 
+    var day = date.getDay();
+    if (day == 0) { date.setTime(date.getTime() - (60*60*1000) * 24 * 6) }
+    else if (day != 1) {
+        for (var i = 0; i < day-1; i++) {
+            date.setTime(date.getTime() - (60*60*1000) * 24)
+        }
+    }
 
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
@@ -20,5 +33,5 @@ function firstDayWeek(number, year) {
 
 
 
-console.log(firstDayWeek(2, "2000"))
+console.log(firstDayWeek(52, '1000'))
 console.log(firstDayWeek(1, 1000))
