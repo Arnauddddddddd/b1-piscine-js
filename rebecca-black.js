2ed7ce657eff40b36759dcb76dc2b296f5505921
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-05 12:05:51 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ rebecca-black.js
@@ -0,0 +1,6 @@
+function isFriday(date) {return (date.getDay == 5)}
+function isWeekend(date) {return (date.getDay == 6 || date.getDay == 0)}
+function isLeapYear(date) {
+    var leapDate = new Date(date.getYear(), 1, 29);
+    return leapDate.getMonth() === 1 && leapDate.getDate() === 29;
+}
