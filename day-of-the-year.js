141db59105df003681350bf741dd40ace78220fb
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-07 10:34:55 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,4 +1,11 @@
 function dayOfTheYear(date) {
-    let date2 = new Date(date.getYear() + 1900, 0);
-    return Math.round((date2.getTime() - date.getTime()) / (1000 * 3600 * 24));
+    var date2 = new Date(date.getYear(), 0);
+    var count = 1
+    while (date2.getMonth() != date2.getMonth() && date2.getDate() != date2.getDate()) {
+        count++
+        date2.setDate(new Date (date2.getYear(), date2.getMonth(), date2.getDate(), date2.getHours() + 24))
+    }
+    return count
 }
+
+console.log(dayOfTheYear(new Date('0001-01-01')))
