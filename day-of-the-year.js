79c601e27b564bdd5ba72205a2db11dc15db5f48
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-07 11:14:02 +0100

    qzifjs<emofiksefiw

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,11 +1,11 @@
 function dayOfTheYear(date) {
     var date2 = new Date(date.getYear());
     var count = 0;
-    // console.log(date2.getMonth())
-    // console.log(date2.getDate())
+    console.log(date2.getMonth())
+    console.log(date2.getDate())
 
-    // console.log(date.getMonth())
-    // console.log(date.getDate())
+    console.log(date.getMonth())
+    console.log(date.getDate())
     while (!(date2.getMonth() == date.getMonth() && date2.getDate() == date.getDate())) {
         count++;
         date2.setDate(date2.getDate() + 1);
@@ -13,6 +13,6 @@ function dayOfTheYear(date) {
     return count;
 }
 
-// console.log(dayOfTheYear(new Date('1600-12-31')))
-// console.log(dayOfTheYear(new Date('1664-08-09')))
+console.log(dayOfTheYear(new Date('1600-12-31')))
+console.log(dayOfTheYear(new Date('1664-08-09')))
 console.log(dayOfTheYear(new Date('0001-01-01')))
