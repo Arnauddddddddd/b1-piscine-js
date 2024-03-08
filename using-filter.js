ddf57f189efb10067370997b658da45e72706011
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-08 12:00:34 +0100

    salut

--- using-filter.js
+++ using-filter.js
@@ -7,12 +7,28 @@ function filterStartVowel(arr) {
 }
 
 function filter5Vowels(arr) {
-    return arr.filter((word) => (
-        word.includes("a") &&
-        word.includes("e") &&
-        word.includes("i") &&
-        word.includes("o") &&
-        word.includes("u")
-      ));
+    var txt = arr.filter((word) => word);
+    var arr2 = []
+    for (var word of txt) {
+        var count = 0;
+        for (var i = 0; i < word.length; i++) {
+            if (word[i].includes("a") || word[i].includes("e") ||word[i].includes("i") || word[i].includes("o") || word[i].includes("u")) {
+                count++
+            }
+        }
+        if (count >= 5) { arr2.push(word)}
+    }
+    return arr2
 }
 
+
+
+console.log(filter5Vowels([
+    'California',
+    'Louisiana',
+    'North Carolina',
+    'South Carolina',
+    'South Dakota',
+    'West Virginia',
+    'weest'
+  ]))
