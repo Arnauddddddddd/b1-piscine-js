4d3f8e315c0b5c15870a5ab1fe7b0b5d685642e0
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-08 09:22:51 +0100

    dd

--- using-map.js
+++ using-map.js
@@ -1,3 +1,39 @@
 function citiesOnly(arr) {
-    return arr.keys();
+    var arr2 = [];
+    for (var i = 0; i < arr.length; i++) {
+        arr2.push(arr[i].city)
+    }
+    return arr2;
 }
+
+
+function upperCasingStates(arr) {
+  var arr2 = []
+  var arr3 = []
+  for (var i = 0; i < arr.length; i++) {
+    var txt = ""
+    arr2 = (arr[i].split(" "))
+    for (var j = 0; j < arr2.length; j++) {
+      if (j > 0) { txt += " "}
+      txt +=  arr2[j][0].toUpperCase() + arr2[j].slice(1);
+    }
+    arr3.push(txt)
+  }
+  return arr3         
+}
+
+
+console.log(upperCasingStates(['alabama', 'new jersey']) // -> ['Alabama', 'New Jersey']
+)
+
+
+// console.log(citiesOnly([
+//     {
+//       city: 'Los Angeles',
+//       temperature: '  101 °F   ',
+//     },
+//     {
+//       city: 'San Francisco',
+//       temperature: ' 84 ° F   ',
+//     },
+//   ]))
