614b2c1b56e94d358e6b87a02836950ed8e9e482
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-12 12:23:55 +0100

    salut à tous, c'est la Salle

--- using-reduce.js
+++ using-reduce.js
@@ -1,13 +1,14 @@
 function adder(arr, value = 0) {
-    return arr.reduce((accumulator, currentValue, value) => accumulator + currentValue);
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue, value);
 }
 
 function sumOrMul(arr, value = 0) {
-    return arr.reduce((accumulator, currentValue) => {
+    var result = arr.reduce((accumulator, currentValue) => {
         if (currentValue % 2 == 0 ) return accumulator * currentValue;
         if (currentValue % 2 == 1 ) return accumulator + currentValue;
         return accumulator;
       }, value);
+    return result
 }
 
 
@@ -16,4 +17,4 @@ function funcExec(arr) {
 }
 
 
-console.log(sumOrMul(adder([9, 24, 7, 11, 3], 10)))
+console.log(adder([9, 24, 7, 11, 3], 10))
