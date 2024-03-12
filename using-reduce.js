cdc39fcf81286f95feedf8567ef5af034e77e73d
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-12 12:20:10 +0100

    salut à tous, c'est la Salle

---
+++ using-reduce.js
@@ -0,0 +1,20 @@
+function adder(arr) {
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+
+function sumOrMul(arr, value = 0) {
+    result = arr.reduce((accumulator, currentValue) => {
+        if (currentValue % 2 == 0 ) return accumulator * currentValue;
+        if (currentValue % 2 == 1 ) return accumulator + currentValue;
+        return accumulator;
+      }, value);
+    return result
+}
+
+
+function funcExec(arr) {
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+}
+
+
+console.log(sumOrMul([1, 2, 3, 5, 8], 5))
