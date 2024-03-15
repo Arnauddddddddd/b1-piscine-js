8d0b07a0140e8527b0815ef29131533b5d324a91
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 11:40:20 +0100

    ddd

--- reduce.js
+++ reduce.js
@@ -1,12 +1,24 @@
 function fold(arr, func, value) {
     var list = [];
+    var entier = 0;
     list.push(value)
     for (var i = 0; i < arr.length; i++) {
         list.push(arr[i])
     }
-    return func(list);
+    for (var i = 0; i < list.length; i++) {
+        entier += func(list[i])
+    }
+    return entier;
 }
+
+function adder(arr, value = 0) {
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue, value);
+}
+
 
 function reduce(arr) {
     var list = [];
 }
+
+
+console.log(fold([3,4,3], ))
