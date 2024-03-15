53d6e8363c549a5923828afb24662934360e767c
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 10:27:28 +0100

    ddd

--- for-each.js
+++ for-each.js
@@ -1,6 +1,6 @@
 function forEach(arr, func = null) {
     var list2 = []
-    for (var i = func; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         if (func != null) {
             list2.push(arr[i]);
         }
