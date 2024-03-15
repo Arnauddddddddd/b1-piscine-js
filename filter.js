e013093b36d0e8bcc8b7ae26802e3be821cd33f1
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 11:20:12 +0100

    ddd

--- filter.js
+++ filter.js
@@ -7,3 +7,13 @@ function filter(arr, func) {
     }
     return list
 }
+
+function reject(arr, func) {
+    var list = [] 
+    for (var i = 0; i < arr.length; i++) {
+        if (!(func(arr[i], i, arr))) {
+            list.push(arr[i])
+        }
+    }
+    return list
+}
