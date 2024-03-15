c66d8002f8b53690c0390ad5cc8f1cef3f9f0def
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 11:13:22 +0100

    ddd

--- flat.js
+++ flat.js
@@ -1,13 +1,19 @@
-function flat(arr, numb) {
-    var arr2 = [];
-    if (numb > 0) {
-        for (var i = 0; i < arr.length; i++) {
-            if (Array.isArray(arr[i])) {
-                arr2 = arr2.slice(i, )
+function flat(arr, n = 1) {
+    if (n == 0) return arr
+    var a = [];
+    var noArr = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            noArr = false;
+            for (let j = 0; j < arr[i].length; j++) {
+                a.push(arr[i][j])
             }
+
+        } else {
+            a.push(arr[i])
         }
     }
-    return arr2;
-}
 
-console.log(flat([1,2,3,4], 4))
+    if (noArr) return a
+    return flat(a, n-1)
+}
