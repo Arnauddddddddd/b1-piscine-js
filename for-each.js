229bf8410d4f880c5fa5be4f47ae4f19740aa564
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 10:06:01 +0100

    ddd

--- for-each.js
+++ for-each.js
@@ -1,5 +1,7 @@
-function forEach(arr, func) {
-    for (var i = 0; i < arr.length; i++) {
-        func(arr[i]);
+function forEach(arr, index) {
+    var list2 = []
+    for (var i = index; i < arr.length; i++) {
+        list2.push(arr[i])
     }
+    return list2;
 }
