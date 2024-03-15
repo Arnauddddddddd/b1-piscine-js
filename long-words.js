2ff67e6bfb96bc6ebd7047eeb0dfbd6b73d20310
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 09:57:06 +0100

    ddd

--- long-words.js
+++ long-words.js
@@ -1,3 +1,11 @@
 function longWords(arr) {
     return arr.every((currentValue) => currentValue.length < 5);
 }
+
+function oneLongWord(arr) {
+    return arr.every((currentValue) => currentValue.length >= 10);
+}
+
+function noLongWords(arr) {
+    return arr.every((currentValue) => currentValue.length < 7);
+}
