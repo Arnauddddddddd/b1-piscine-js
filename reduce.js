26fd855d074bb781c100b7544a31bd9594c5bb07
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 11:58:01 +0100

    ddd

--- reduce.js
+++ reduce.js
@@ -13,7 +13,17 @@ function fold(arr, func, value) {
 }
 
 function foldRight(arr, func, value) {
-    return fold(arr.reverse(), func, value);
+    var list = [];
+    var entier = 0
+    list.push(value)
+    for (var i = arr.length-1; i >= 0; i--) {
+        list.push(arr[i])
+    }
+    for (var j = 1; j < list.length; j++) {
+        list[j] = func(list[j-1], list[j])
+        entier = list[j]
+    }
+    return entier;
 }
 
 // function adder(arr, value = 0) {
