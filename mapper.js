2766e7eedc3d87604b4c0ac1cb4fcc112219056d
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 11:07:20 +0100

    ddd

--- mapper.js
+++ mapper.js
@@ -9,7 +9,7 @@ function map(arr, func = null) {
 function flatMap(arr, func = null) {
     var list = [] 
     for (var i = 0; i < arr.length; i++) {
-        list.push(func(arr[i], i, arr))
+        list.push(func(arr[i]))
     }
     return list
 }
