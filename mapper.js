13c9c3178634bc43885cc632ed386cb1495e9db9
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-15 11:15:09 +0100

    ddd

--- mapper.js
+++ mapper.js
@@ -13,3 +13,23 @@ function flatMap(arr, func = null) {
     }
     return flat(list, Infinity)
 }
+
+function flat(arr, n = 1) {
+    if (n == 0) return arr
+    var a = [];
+    var noArr = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            noArr = false;
+            for (let j = 0; j < arr[i].length; j++) {
+                a.push(arr[i][j])
+            }
+
+        } else {
+            a.push(arr[i])
+        }
+    }
+
+    if (noArr) return a
+    return flat(a, n-1)
+}
