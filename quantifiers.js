f867a11067069197836c99cecb76b6f3fa6d6e63
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-25 11:12:23 +0100

    aaaaaaaaaa

--- quantifiers.js
+++ quantifiers.js
@@ -6,3 +6,12 @@ function every(array, func) {
     }
     return true;
 }
+
+function some(array, func) {
+    for (var i = 0; i < array.length; i++) {
+        if (func(array[i])) {
+            return true;
+        }
+    }
+    return false;
+}
