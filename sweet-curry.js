e39bbb54ea51625831baf3ee502abc4e2f8a0ca8
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-26 11:30:47 +0100

    ss

--- sweet-curry.js
+++ sweet-curry.js
@@ -1,11 +1,21 @@
 function mult2(a) {
-    return a*a;
+    return function (b) {
+        return a * b;
+    }
 }
 
 function add3(a) {
-    return a+a+a;
+    return function (b) {
+        return a + b;
+    }
 }
 
 function sub4(a) {
-    return a-a-a-a;
+    return function (b) {
+        return function (c) {
+            return function (d) {
+                return a + b;
+            }
+        }
+    }
 }
