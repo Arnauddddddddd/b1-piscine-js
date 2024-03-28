f9b090eb81378b10ccccbfc5c18e83f271eb8faa
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-28 09:32:34 +0100

    ss

---
+++ hasCity.js
@@ -0,0 +1,9 @@
+function country(country, list) {
+    return function (city) {
+        if (list.includes(city)) {
+            return city + " is a city from " + country;
+        }
+        return city + " is not a city from " + country;
+    }
+}
+
