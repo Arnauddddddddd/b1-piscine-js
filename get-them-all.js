1ac8c2cd67421c026c369ba1261f82a458a0ce63
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-28 11:58:59 +0100

    ed

--- get-them-all.js
+++ get-them-all.js
@@ -4,7 +4,7 @@ export const getArchitects = () => {
 }
 export const getClassical = () => {
     //return [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
-    return [document.getElementsByClassName("classical"), document.getElementsByClassName("classical")];
+    return [document.getElementsByClassName("classical"), document.getElementsByClassName(":not(classical)")];
 }
 
 export const getActive = () => {
