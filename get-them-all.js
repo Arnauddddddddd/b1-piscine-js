6f8dd37d9b2c3f0ba2e268d1ecfd24015feb67a3
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-28 11:36:22 +0100

    ed

--- get-them-all.js
+++ get-them-all.js
@@ -1,18 +1,18 @@
 export const getArchitects = () => {
-    return [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
+    [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
 export const getClassical = () => {
-    return [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
+    [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
 
 export const getActive = () => {
-    return [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
+    [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
 
 export const getBonannoPisano = () => {
-    return [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
+    [['BillieElliott', 'BillieElliott'],  ['BillieElliott', 'BillieElliott']]
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
