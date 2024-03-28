888d5701915343b310ba37c5da84b3ba3dd33667
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-28 11:20:46 +0100

    ed

--- get-them-all.js
+++ get-them-all.js
@@ -1,18 +1,18 @@
 export const getArchitects = () => {
-    return ['BillieElliott']
+    return ['BillieElliott', 'BillieElliott']
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
 export const getClassical = () => {
-    return ['BillieElliott']
+    return ['BillieElliott', 'BillieElliott']
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
 
 export const getActive = () => {
-    return ['BillieElliott']
+    return ['BillieElliott', 'BillieElliott']
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
 
 export const getBonannoPisano = () => {
-    return ['BillieElliott']
+    return ['BillieElliott', 'BillieElliott']
     //return [document.getElementById("LolaDunam"), document.getElementById("LolaDunam")];
 }
