9af3ce83abe3418076442f6feab6fe3ffef1d01d
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-28 10:52:45 +0100

    ed

---
+++ get-them-all/get-them-all.js
@@ -0,0 +1,15 @@
+export function getArchitects() {
+    return [document.getElementsByTagName("a"), !document.getElementsByTagName("a")];
+}
+
+export function getClassical() {
+    return [document.getElementsByTagName(a), document.getElementsByTagName(a)];
+}
+
+export function getActive() {
+    return [document.getElementsByTagName(a), document.getElementsByTagName(a)];
+}
+
+export function getBonannoPisano() {
+    return [document.getElementsByTagName(a), document.getElementsByTagName(a)];
+}
