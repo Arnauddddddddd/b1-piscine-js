efb9b351f98185f41050b266a3a6830288fb36a9
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-29 12:06:46 +0100

    letters

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,19 +1,17 @@
 export const generateLetters = () => {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var fontsize = 11
-    var fontweight = 300;
-    for (var i = 1; i <= 120; i++) {
+    var size = 11
+    var weight = 300;
+    for (var i = 0; i < 120; i++) {
         let div = document.createElement("div");
         let letter = document.createTextNode(alphabet.charAt(Math.floor(Math.random() * 26)));
         div.appendChild(letter);
         div.id = 'letter-' + i.toString();
-
-        div.style.fontSize = fontsize + "px";
-        if (i == 40) { fontweight = 400}
-        if (i == 80) { fontweight = 600}
-        div.style.fontWeight = fontweight + "px";
+        div.style.fontSize = size + "px";
+        if (i == 40) { weight = 400}
+        if (i == 80) { weight = 600}
+        div.style.fontWeight = weight + "px";
         document.body.appendChild(div)
         fontsize++
-
     }
 }
