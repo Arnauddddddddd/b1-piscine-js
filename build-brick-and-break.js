487a3f3fa3cd8a7aa4ce5c4d246d0f94d6332f95
parnaud <arnaud.pfeillefort@ynov.com>
2024-03-29 10:18:01 +0100

    salut

--- build-brick-and-break.js
+++ build-brick-and-break.js
@@ -1,12 +1,12 @@
 export const build = (nbr) => {
-    let i = 0;
+    let i = 1;
     let interval = setInterval(function() {
         let div = document.createElement("div");
         const brick = document.createElement('brick');
         div.append(brick);
         div.id = 'brick-' + i.toString();  
         document.body.appendChild(div)
-        if (i == nbr) {
+        if (i > nbr) {
             clearInterval(interval)
         }
         i++
