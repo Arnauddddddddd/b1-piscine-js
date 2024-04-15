92aabfaaecbe5dfa07946642daf1c2fb82c8dd85
parnaud <arnaud.pfeillefort@ynov.com>
2024-04-15 10:07:24 +0200

    fr

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -4,18 +4,22 @@ var index = 0;
 var change = 1;
 
 export const pimp = () => {
-    document.querySelector("button").classList.toggle("unpimp");
     if (change != -1) {
         document.querySelector("button").classList.add(styles[index]);
     } else {
-        document.querySelector("button").classList.remove(styles[index]);
         document.querySelector("button").classList.toggle("unpimp");
+        document.querySelector("button").classList.toggle("unpimp");
+        document.querySelector("button").classList.remove(styles[index]);
     }
     console.log(index);
     index += change;
     if (index == styles.length) {
         change = -1;
         index--;
-    } 
+    }
+    if (index == styles.length-2 && change == -1) {
+        document.querySelector("button").classList.toggle("unpimp");
+    }
+    console.log(document.querySelector("button").classList);
 
 }
